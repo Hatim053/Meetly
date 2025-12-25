4b03fd83538a3db82fa420fbbd7307784d98942e
Hatim Hussain <hatimrockst@gmail.com>
2025-12-25 20:44:53 +0530

    deployement issue resolved

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import './App.css'
 import { Outlet } from 'react-router-dom'
 import Header from './components/Header/Header.jsx'
 import Banner from './components/Banner/Banner.jsx'
